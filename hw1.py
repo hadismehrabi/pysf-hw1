8372987a1a834a0032efd9f4d4cebf14cda545b9
hadismehrabi <86637179+hadismehrabi@users.noreply.github.com>
2021-06-29 16:49:44 +0430

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,14 @@
+import colorama
+from colorama import Fore, Back, Style
+print(Back.BLUE +'                  ',Back.GREEN +'',Back.BLUE +'                  ')
+print(Back.BLUE +'                ',Back.GREEN +'    ',Back.BLUE +'                ')
+print(Back.BLUE +'              ',Back.GREEN +'        ',Back.BLUE +'              ')
+print(Back.BLUE +'            ',Back.GREEN +'            ',Back.BLUE +'            ')
+print(Back.BLUE +'          ',Back.GREEN +'                ',Back.BLUE +'          ')
+print(Back.BLUE +'       ',Back.GREEN +'                      ',Back.BLUE +'       ')
+print(Back.BLUE +'       ',Back.RED +'                      ',Back.BLUE +'       ')
+print(Back.BLUE +'       ',Back.RED +'            ',Back.WHITE +'    ',Back.RED +'    ',Back.BLUE +'       ')
+print(Back.BLUE +'       ',Back.RED +'        ',Back.YELLOW +'  ',Back.RED +'',Back.WHITE +'    ',Back.RED +'    ',Back.BLUE +'       ')
+print(Back.BLUE +'       ',Back.RED +'        ',Back.YELLOW +'  ',Back.RED +'',Back.RED +'         ',Back.BLUE +'       ')
+print(Back.BLUE +'       ',Back.RED +'        ',Back.YELLOW +'  ',Back.RED +'',Back.RED +'         ',Back.BLUE +'       ')
+print(Back.BLUE +'       ',Back.RED +'                      ',Back.BLUE +'       ')
